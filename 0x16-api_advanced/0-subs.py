1a344e72051a6e54659464f1124ec63b543c8c20
Salmarh1 <Salmarhadi5@g:mail.com>
2024-05-10 12:34:35 +0000

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
+"""A function that queries the Reddit API and returns the number of subscribers"""
 import requests
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyAPI/0.1"}
-    response = requests.get(url, headers=headers)
-    
-    # Check for errors
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    elif response.status_code == 404:
-        return 0  # Subreddit not found
-    else:
-        return -1  # Other error occurred
+    """returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
 
-if __name__ == "__main__":
-    subreddit = input("Enter subreddit: ")
-    print(number_of_subscribers(subreddit))
+# Test calls
+print(number_of_subscribers("existingsubreddit"))
+print(number_of_subscribers("nonexistingsubreddit"))
