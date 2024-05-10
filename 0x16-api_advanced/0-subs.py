bad31a6faa8fea7036933cbcd4974b8d2523ec79
Salmarh1 <Salmarhadi5@g:mail.com>
2024-05-10 12:44:23 +0000

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of subscribers"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    """returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    print("Response status code:", response.status_code)
-    print("Response content:", response.content)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
 
-# Test calls
-print(number_of_subscribers("existingsubreddit"))
-print(number_of_subscribers("nonexistingsubreddit"))
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
